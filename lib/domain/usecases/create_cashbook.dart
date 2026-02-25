afea55c495052a6773f7d1e5c85067da7bfade83
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-25 21:58:29 +0530

    Update create_cashbook.dart

--- lib/domain/usecases/create_cashbook.dart
+++ lib/domain/usecases/create_cashbook.dart
@@ -1,12 +1,22 @@
 import 'package:kashly/domain/repositories/cashbook_repository.dart';
 import 'package:kashly/domain/entities/cashbook.dart';
+import 'package:kashly/core/error/exceptions.dart';
+import 'package:kashly/core/error/failures.dart';
+import 'package:dartz/dartz.dart'; // Add dartz to pubspec for Either if not already (dev_dependencies)
 
 class CreateCashbookUseCase {
   final CashbookRepository repository;
 
   CreateCashbookUseCase(this.repository);
 
-  Future<void> call(Cashbook cashbook) async {
-    await repository.createCashbook(cashbook);
+  Future<Either<Failure, void>> call(Cashbook cashbook) async {
+    try {
+      await repository.createCashbook(cashbook);
+      return const Right(null);
+    } on CacheException catch (e) {
+      return Left(CacheFailure(e.message));
+    } on Exception catch (e) {
+      return Left(CacheFailure('Unexpected error: $e'));
+    }
   }
 }
