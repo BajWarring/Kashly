f205e04da4234f88aa3f43cabca58b06bcb2413f
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-28 08:29:25 +0530

    Delete lib directory

--- lib/data/repositories/cashbook_respository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:kashly/domain/entities/cashbook.dart';
-
-abstract class CashbookRepository {
-  Future<void> createCashbook(Cashbook cashbook);
-  Future<List<Cashbook>> getCashbooks();
-  // Add more
-}
