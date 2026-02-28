adc81d86b6390e0df6bc0a90d5888f55c465a80c
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-28 08:28:48 +0530

    Delete android directory

--- android/app/src/main/kotlin/com/kashly/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.kashly.app
-
-// ✅ V2 embedding — replaces the deleted io.flutter.app.FlutterActivity (v1)
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
